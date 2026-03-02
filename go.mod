eef8e8c38f8ecfb928daaf7242503fd9769a3bc0
d0ctr <mr.den.vi@gmail.com>
2026-03-02 23:04:11 +0000

    ping

--- go.mod
+++ go.mod
@@ -1,12 +1,9 @@
 module github.com/d0ctr/bldbr-bot-go
 
-go 1.21
+go 1.25.0
 
 require (
-	github.com/dotenv-org/godotenvvault v0.6.0
-	gopkg.in/telebot.v3 v3.1.3
+	github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.34 // indirect
+	github.com/dotenv-org/godotenvvault v0.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
-
-require github.com/joho/godotenv v1.5.1 // indirect
-
-replace gopkg.in/telebot.v3 => github.com/d0ctr/telebot v0.0.0-20230820223142-a42b93b462ff
--- main.go
+++ main.go
@@ -2,38 +2,88 @@ package main
 
 import (
 	"os"
-	"strconv"
-
-	"github.com/d0ctr/bldbr-bot-go/common/commands"
-	"github.com/d0ctr/bldbr-bot-go/common/logger"
-	"github.com/d0ctr/bldbr-bot-go/telegram"
+	"log/slog"
+	"net/http"
 
 	"github.com/dotenv-org/godotenvvault"
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
 )
 
+const (
+	TELEGRAM_TOKEN = "TELEGRAM_TOKEN"
+	ENV = "ENV"
+)
+
+func createLogger() *slog.Logger {
+	opts := &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if (a.Key == slog.TimeKey) {
+				return slog.Time(a.Key, a.Value.Time().UTC())
+			}
+
+			return a
+		},
+	};
+	handler := slog.NewJSONHandler(os.Stdout, opts);
+	logger := slog.New(handler);
+	slog.SetDefault(logger);
+	return logger;
+}
+
+func createTgBot() (*gotgbot.Bot, *ext.Dispatcher, *ext.Updater, error) {
+	tgToken := os.Getenv(TELEGRAM_TOKEN)
+	// env := os.Getenv(ENV)
+
+	opts := &gotgbot.BotOpts{
+		BotClient: &gotgbot.BaseBotClient{
+			Client: http.Client{},
+			UseTestEnvironment: false, // env == "local" || env == "dev",
+		},
+	}
+
+	bot, err := gotgbot.NewBot(tgToken, opts)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	dispatcher := ext.NewDispatcher(&ext.DispatcherOpts{
+		Logger: slog.Default(),
+	})
+
+	updater := ext.NewUpdater(dispatcher, &ext.UpdaterOpts{
+		Logger: slog.Default(),
+	})
+
+	return bot, dispatcher, updater, nil
+}
+
 func main() {
-	logger.RootLogger.INFO("Starting...")
-	err := godotenvvault.Load()
-	if err != nil {
-		logger.RootLogger.FATAL("error loading dotenv: ", err)
+	logger := createLogger()
+	if err := godotenvvault.Load(); err != nil {
+		logger.Error("failed to acquire env variables", slog.Any("error", err));
 	}
 
-	logger.SetRootLogger(logger.CreateLogger(&logger.LoggerOptions{
-		Module:    os.Getenv("ENV"),
-		IsColored: func() bool { v, _ := strconv.ParseBool(os.Getenv("COLORED_LOG")); return v }(),
-	}))
-
-	l := logger.RootLogger.Child(&logger.ChildLoggerOptions{Module: "main"})
-
-	bot, err := telegram.CreateBot()
+	bot, dispatcher, updater, err := createTgBot()
 	if err != nil {
-		l.FATAL("telegram bot didn't start: ", err)
+		logger.Error("failed to start tg bot", slog.Any("error", err))
 	}
 
-	bot.AddHandler("/ping", commands.Ping)
-	bot.AddHandler("/info", commands.Info)
+	dispatcher.AddHandler(handlers.NewCommand(
+		"ping",
+		func(bot *gotgbot.Bot, ctx *ext.Context) error {
+			ctx.EffectiveMessage.Reply(bot, "<code>pong</code>", &gotgbot.SendMessageOpts{
+				ParseMode: "HTML",
+			})
+			return nil
+		},
+	))
 
-	bot.SetMyCommands()
+	updater.StartPolling(bot, nil)
 
-	bot.Start()
+	updater.Idle()
+	
+	logger.Info("bot has finished")
 }
